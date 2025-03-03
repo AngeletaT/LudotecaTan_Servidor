a5c00d446e21aea6fff63c154005d88a0172212e
Angela Torro <angeletatorro98@gmail.com>
2025-03-03 14:25:06 +0100

    add initial authors to data.sql for enhanced game catalog

--- tutorial/src/main/resources/data.sql
+++ tutorial/src/main/resources/data.sql
@@ -2,3 +2,11 @@ INSERT INTO category(name) VALUES
 ('Eurogames'),
 ('Ameritrash'),
 ('Familiar');
+
+INSERT INTO author(name, nationality) VALUES 
+('Alan R. Moon', 'US'),
+('Vital Lacerda', 'PT'),
+('Simone Luciani', 'IT'),
+('Perepau Llistosella', 'ES'),
+('Michael Kiesling', 'DE'),
+('Phil Walker-Harding', 'US');
